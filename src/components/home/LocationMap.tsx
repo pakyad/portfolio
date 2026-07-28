5d75751a3eb6dc25100463877980cc6149cfb9d8
Muhammad Iyad Iman <iyadmohmadnazri@gmail.com>
2026-07-28 13:07:40 +0800

    fix: contact link font, correct LocationMap arrow origin

--- src/components/home/LocationMap.tsx
+++ src/components/home/LocationMap.tsx
@@ -63,7 +63,7 @@ export function LocationMap() {
       <div ref={mapRef} className="location-map">
         <InteractiveMap positions={DOT_POSITIONS} penang={PENANG} />
       </div>
-      <p className="info-map-label"><span ref={labelRef}>Based in Penang.</span> <span data-sound="cry" className="info-map-status">{STATUS}</span></p>
+      <div className="info-map-label-row"><span ref={labelRef} className="info-map-label">Based in Penang.</span><span data-sound="cry" className="info-map-status">{STATUS}</span></div>
       {arrow && (
         <svg
           className="map-arrow"
